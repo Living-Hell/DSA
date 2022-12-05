5500b2645e3cefb39ce9830fe68e3abd95251ba9
Shushant Kumar <s.living.hell@gmail.com>
2022-12-05 19:55:58 +0530

    Adding Data Structures Folder

---
+++ CodeHere.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Code{
+public:
+
+    //Write Function here
+    void function(){
+
+        //Write your code here
+
+    }
+
+};
+
+int main(){
+
+    //Call and check the function here
+    return 0;
+}
