f1d19e1ce27f5e42ab6bf5d03338182e94e6eb3c
Shushant Kumar <s.living.hell@gmail.com>
2022-12-06 12:16:12 +0530

    Adding sort012 for Array in DS

--- Data Structures/Array.cpp
+++ Data Structures/Array.cpp
@@ -43,6 +43,27 @@ public:
         return pq.top();
     }
 
+    //To sort array containing 0,1,2
+    vector<int> sort012(vector<int> arr)
+    {
+        int n = arr.size();
+        int count0 = 0, count1 = 0, count2 = 0;
+
+        for(int i=0; i<n; i++){
+            if(arr[i] == 0) count0++;
+            else if(arr[i] == 1) count1++;
+            else count2++;
+        }
+        
+        for(int i=0; i<count0; i++) arr[i] = 0;
+        for(int i=count0; i<count0+count1; i++) arr[i] = 1;
+        for(int i=count0+count1; i<n; i++) arr[i] = 2;
+
+        return arr;
+    }
+
+
+
     //To print the array
     void print(vector<int> v){
         for(int i=0; i<v.size(); i++) 
