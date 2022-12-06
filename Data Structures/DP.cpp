a6c5b50f1983aca6e52444eec6c04bc59e7904b3
Shushant Kumar <s.living.hell@gmail.com>
2022-12-06 15:08:50 +0530

    Adding Factorial in DS/DP

--- Data Structures/DP.cpp
+++ Data Structures/DP.cpp
@@ -3,14 +3,27 @@ using namespace std;
 
 class DP{
 public:
+    const unsigned int mod = 1000000007;
 
-    bool CycleDetection(){}
+    long long int Factorial(int n){
+        long long int factorial[n+1] = {0};
+        if(n>=0){
+            factorial[0] = 1;
+            for(int i=1; i<=n; i++){
+                factorial[i] = ((i%mod)*(factorial[i-1]%mod))%mod;
+            }
+            return factorial[n];
+        }
+    }
 
 };
 
 int main(){
 
     DP dp;
+
+    //cout<<dp.Factorial(13);
+    //cout<<dp.nCr(14,3);
 
     return 0;
 }
