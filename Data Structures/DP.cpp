a6398289901e43fd1e41ff7d56846463a54cfd53
Shushant Kumar <s.living.hell@gmail.com>
2022-12-15 00:52:30 +0530

    Adding LongestRepeatingSubsequence in DS/DP

--- Data Structures/DP.cpp
+++ Data Structures/DP.cpp
@@ -272,6 +272,23 @@ public:
         return dp[x][y];
     }
 
+    int LongestRepeatingSubsequence(string s){
+        int n = s.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(i>0 && j>0){
+                    if(s[i-1] == s[j-1] && i!=j)
+                        dp[i][j] = dp[i-1][j-1]+1;
+                    else
+                        dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][n];
+    }
+
 };
 
 int main(){
@@ -293,6 +310,7 @@ int main(){
     //cout<<dp.countWays(5000,100);
     //cout<<dp.maximizeTheCuts(4,2,2,1);
     //cout<<dp.lcs("ABCDGH", "AEDFHR");
+    cout<<dp.LongestRepeatingSubsequence("jdzkvjkzcnvkjxcvnkjzczvjk");
 
     return 0;
 }
