4567740d5a27706b1fff756a06f21a798c033564
Shushant Kumar <s.living.hell@gmail.com>
2022-12-14 12:36:10 +0530

    Adding Lowest Common Subsequence in DS/DP

--- Data Structures/DP.cpp
+++ Data Structures/DP.cpp
@@ -254,6 +254,24 @@ public:
         return ret;
     }
 
+    int lcs(string s1, string s2)
+    {
+        int x = s1.size(), y = s2.size();
+        vector<vector<int>> dp(x+1,vector<int>(y+1,0));
+        
+        for(int i=1; i<=x; i++){
+            for(int j=1; j<=y; j++){
+                if(i>0 && j>0){
+                    if(s1[i-1] == s2[j-1])
+                        dp[i][j] = dp[i-1][j-1]+1;
+                    else
+                        dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[x][y];
+    }
+
 };
 
 int main(){
@@ -274,6 +292,7 @@ int main(){
     //cout<<dp.matrixChainMultiplication({1, 2, 3, 4, 3});
     //cout<<dp.countWays(5000,100);
     //cout<<dp.maximizeTheCuts(4,2,2,1);
+    //cout<<dp.lcs("ABCDGH", "AEDFHR");
 
     return 0;
 }
