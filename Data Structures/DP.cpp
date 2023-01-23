0d05ec481534a584979cc8077ec2d4ed6aac007a
Shushant Kumar <s.living.hell@gmail.com>
2023-01-24 02:01:19 +0530

    Adding longestValidParentheses in DS/DP

--- Data Structures/DP.cpp
+++ Data Structures/DP.cpp
@@ -928,6 +928,24 @@ public:
         return ans;
     }
 
+    int longestValidParentheses(string s) {
+        int n = s.size(); if(n==0) return 0;
+        vector<int> dp(n,0);
+
+        for(int i=1; i<n; i++){
+            if(s[i] == ')'){
+                int prev_left = i-1-dp[i-1];
+                if(prev_left>=0 && s[prev_left] == '('){
+                    dp[i] = dp[i-1] + 2;
+                    if(prev_left>0){
+                        dp[i] += dp[prev_left-1];
+                    }
+                }
+            }
+        }
+        return *max_element(dp.begin(),dp.end());
+    }
+
 
 };
 
@@ -980,6 +998,7 @@ int main(){
     //cout<<dp.isInterleave("XY", "X", "XYX");
     //cout<<dp.maxProfit(2,{10,22,5,75,65,80});
     //cout<<dp.maximalRectangle({{0, 1, 1, 0},{1, 1, 1, 1},{1, 1, 1, 1},{1, 1, 0, 0}});
+    //cout<<dp.longestValidParentheses("()(())()())()()");
 
     return 0;
 }
