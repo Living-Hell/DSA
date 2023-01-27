11054bc0f97ffc1f7756e0f9e12a6095945da62e
Shushant Kumar <s.living.hell@gmail.com>
2023-01-27 16:04:34 +0530

    Adding topoSort in Algorithms/Graph

---
+++ Algorithms/Graph/Kahn'sAlgorithm.cpp
@@ -0,0 +1,30 @@
+//Function to return list containing vertices of a 
+//Directed Acyclic Graph (DAG) in Topological order. 
+
+vector<int> topoSort(int v, vector<int> adj[]) {
+    vector<int> indegree(v,0),ans;
+    queue<int> q;
+    
+    for(int i=0; i<v; i++){
+        for(int j:adj[i]){
+            indegree[j]++;
+        }
+    }
+    for(int i=0; i<v; i++){
+        if(indegree[i] == 0)
+            q.push(i);
+    }
+    
+    while(!q.empty()){
+        int top = q.front();
+        q.pop();
+        ans.push_back(top);
+        
+        for(int i:adj[top]){
+            indegree[i]--;
+            if(indegree[i] == 0)
+                q.push(i);
+        }
+    }
+    return ans;
+}
