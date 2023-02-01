8ffcdb8df76d7fd8d8b8e4421b50d5cef2a2da4a
Shushant Kumar <s.living.hell@gmail.com>
2023-02-02 00:00:23 +0530

    Adding wordLadderLength in DS/Graph

--- Data Structures/Graph.cpp
+++ Data Structures/Graph.cpp
@@ -312,6 +312,41 @@ public:
         return ex_edges >= comp? comp : -1;
     }
 
+    int wordLadderLength(string startWord, string targetWord, vector<string>& wordList) {
+        auto it = find(wordList.begin(), wordList.end(), targetWord);
+        if(it == wordList.end()) return 0;
+
+        queue<pair<string,int>> q;
+        unordered_set<string> st;
+        int n = wordList.size();
+
+        for(string s:wordList) st.insert(s);
+        q.push(make_pair(startWord,1));
+
+        while(!q.empty()){
+            auto top = q.front();
+            q.pop();
+
+            for(int i=0; i<top.first.size(); i++){
+                string s = top.first;
+
+                for(int j=97; j<=122; j++){
+                    if(j==s[i]) continue;
+
+                    s[i] = j;
+
+                    if(st.find(s)!=st.end()){
+                        q.push(make_pair(s,top.second+1));
+                        st.erase(s);
+                    }
+                        
+                    if(s == targetWord) return top.second+1;
+                }
+            }
+        }
+        return 0;
+    }
+
 };
 
 int main()
