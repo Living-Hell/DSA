22764e74e180a32cc34ce43374ef22f3b4760c12
Shushant Kumar <s.living.hell@gmail.com>
2023-02-02 17:35:37 +0530

    Adding topoSort in DS/Graph

--- Data Structures/Graph.cpp
+++ Data Structures/Graph.cpp
@@ -91,6 +91,35 @@ public:
             cout<<endl;
         }
     }
+
+    //Find any Topological Sorting of the Directed Acyclic Graph (DAG) with V vertices and E edges.
+    vector<int> topoSort(int v, vector<int> adj[]) {
+	    vector<int> indegree(v,0),ans;
+	    queue<int> q;
+	    
+	    for(int i=0; i<v; i++){
+	        for(int j:adj[i]){
+	            indegree[j]++;
+	        }
+	    }
+	    for(int i=0; i<v; i++){
+	        if(indegree[i] == 0)
+	            q.push(i);
+	    }
+	    
+	    while(!q.empty()){
+	        int top = q.front();
+	        q.pop();
+	        ans.push_back(top);
+	        
+	        for(int i:adj[top]){
+	            indegree[i]--;
+	            if(indegree[i] == 0)
+	                q.push(i);
+	        }
+	    }
+	    return ans;
+	}
     
     // Function for BFS Traversal of a Graph
     vector<int> BFSTraversal(int v, vector<vector<int>> adj) {
