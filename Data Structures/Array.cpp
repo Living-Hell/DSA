504073248e566614b4468c685dafcd7670790559
Shushant Kumar <s.living.hell@gmail.com>
2023-03-30 16:24:58 +0530

    Adding minSwaps in DS/Array

--- Data Structures/Array.cpp
+++ Data Structures/Array.cpp
@@ -102,6 +102,24 @@ public:
         cout<<endl;
     }
 
+    //Function to find the minimum number of swaps required to sort the array. 
+	int minSwaps(vector<int>&nums){
+        vector<pair<int,int>> v;
+        
+        for(int i=0; i<nums.size(); i++)
+            v.push_back({nums[i],i});
+        sort(v.begin(),v.end());
+        
+        int swaps=0;
+        for(int i=0; i<v.size(); i++){
+            while(i != v[i].second){
+                swap(v[v[i].second], v[i]);
+                swaps++;
+            }
+        }        
+        return swaps;
+	}
+
 };
 
 int main(){
