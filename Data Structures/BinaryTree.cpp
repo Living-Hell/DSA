e770dd8271a78c6e109eda6257bdde024212f498
Shushant Kumar <s.living.hell@gmail.com>
2023-04-13 20:51:29 +0530

    Adding isIsomorphic in DS/BinaryTree

--- Data Structures/BinaryTree.cpp
+++ Data Structures/BinaryTree.cpp
@@ -717,6 +717,16 @@ class BinaryTree{
         return dista+distb;
     }
 
+    // Return True if the given trees are isomotphic. Else return False.
+    bool isIsomorphic(Node *root1,Node *root2){
+        if(!root1 and !root2) return true;
+        if(!root1 or !root2) return false;
+        if(root1->data != root2->data) return false;
+        
+        return (isIsomorphic(root1->left, root2->left) || isIsomorphic(root1->left, root2->right)) and 
+                (isIsomorphic(root1->right, root2->left) || isIsomorphic(root1->right, root2->right));
+    }
+
 };
 
 int main(){
