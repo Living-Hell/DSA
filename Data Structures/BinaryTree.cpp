130cc046261d3d80c17d19daada75c8d44a0e818
Shushant Kumar <s.living.hell@gmail.com>
2023-04-13 17:48:39 +0530

    Adding lca in DS/BinaryTree

--- Data Structures/BinaryTree.cpp
+++ Data Structures/BinaryTree.cpp
@@ -686,6 +686,22 @@ class BinaryTree{
         return MaxSum[root] = ans;
     }
 
+    //Function to return the lowest common ancestor in a Binary Tree.
+    Node* lca(Node* root ,int n1 ,int n2){
+       if(!root) return NULL;
+        
+        if(root->data == n1 or root->data == n2) return root;
+        
+        Node* l = lca(root->left,n1,n2);
+        Node* r = lca(root->right,n1,n2);
+        
+        if(l!= NULL and r!= NULL) return root;
+        else if(l== NULL and r!= NULL) return r;
+        else if(l!= NULL and r== NULL) return l;
+        
+        return NULL;
+    }
+
 };
 
 int main(){
