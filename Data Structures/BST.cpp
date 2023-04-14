153f4f9412451276cf487a856147e85699971035
Shushant Kumar <s.living.hell@gmail.com>
2023-04-14 15:28:47 +0530

    Adding BST.cpp in DS

---
+++ Data Structures/BST.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class BST{
+    public:
+
+    //A binary tree Node
+    struct Node
+    {
+        int data;
+        struct Node* left;
+        struct Node* right;
+        
+        Node(int x){
+            data = x;
+            left = right = NULL;
+        }
+    };
+
+    
+
+};
+
+int main(){
+    BST bst;
+
+    return 0;
+}
