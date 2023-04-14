6c95c5d410162789ce5d0e8198d44b5a2ca9dc14
Shushant Kumar <s.living.hell@gmail.com>
2023-04-14 15:32:47 +0530

    Adding searchRec and SearchItr in DS/BST

--- Data Structures/BST.cpp
+++ Data Structures/BST.cpp
@@ -17,7 +17,24 @@ class BST{
         }
     };
 
-    
+    // Function to search a node in BST (using recursion)
+    bool searchRec(Node* root, int x) {
+        if(root==NULL) return false;
+        if(root->data == x) return true;
+        if(root->data>x) return searchRec(root->left,x);
+        else return searchRec(root->right,x);
+    }
+
+    // Function to search a node in BST (using iteration)
+    bool searchItr(Node* root, int x) {
+        while(root){
+            if(!root) return false;
+            if(root->data == x) return true;
+            else if(root->data>x) root = root->left;
+            else root = root->right;
+        }
+        return false;
+    }
 
 };
 
