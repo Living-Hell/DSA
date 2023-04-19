1e983e2b9f8da9a4b841b29103df1df9d5681a6e
Shushant Kumar <s.living.hell@gmail.com>
2023-04-19 19:38:03 +0530

    Adding LCA in DS/BST

--- Data Structures/BST.cpp
+++ Data Structures/BST.cpp
@@ -122,6 +122,17 @@ class BST{
         populateNext(root->right);
     }
 
+    //Function to find the lowest common ancestor in a BST. 
+    Node* lca2Nodes(Node* root, int n1, int n2){
+        if(!root) return NULL;
+        if(root->data >= n1 and root->data <= n2) return root;
+        else if(root->data < n1) return lca2Nodes(root->right,n1,n2);
+        else return lca2Nodes(root->left,n1,n2);
+    }
+    Node* LCA(Node *root, int n1, int n2){
+    return lca2Nodes(root,min(n1,n2),max(n1,n2));
+    }
+
 };
 
 int main(){
