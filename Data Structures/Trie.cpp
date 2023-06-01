db87dd3267f2637f193f09200f12a2392fd525f1
Shushant Kumar <s.living.hell@gmail.com>
2023-06-01 12:21:25 +0530

    Adding insert and search in DS/Trie

--- Data Structures/Trie.cpp
+++ Data Structures/Trie.cpp
@@ -12,6 +12,50 @@ class TrieNode{
     }
 };
 
+class Trie{
+    public:
+
+    //Function to insert string into TRIE.
+    void insert(TrieNode *root, string key){
+        if(key.size() == 0){ 
+            root->isEndOfWord = true;
+            return;
+        }
+        int ind = key[0] - 'a';
+        TrieNode* child;
+        if(root->children[ind] == NULL){
+            child = new TrieNode();
+            root->children[ind] = child;
+        }
+        else{
+            child = root->children[ind];
+        }
+        insert(child,key.substr(1));
+    }
+
+    //Function to use TRIE data structure and search the given string.
+    bool search(TrieNode *root, string key) {
+        if(key.size() == 0) return root->isEndOfWord;
+        int ind = key[0] - 'a';
+        TrieNode* child;
+        if(root->children[ind] == NULL){
+            child = new TrieNode();
+            return false;
+        }
+        child = root->children[ind];
+        return search(child,key.substr(1));
+    }
+
+};
+
 int main(){
+    Trie trie;
+    TrieNode* root = new TrieNode;
+    // trie.insert(root,"asdds");
+    // trie.insert(root,"abcd");
+    // trie.insert(root,"asd");
+    // cout<<trie.search(root,"abcd");
+    // cout<<trie.search(root,"abd");
+
     return 0;
 }
