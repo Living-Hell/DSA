48298fae8213d82b3788247bd5cdaed8964b227a
Shushant Kumar <s.living.hell@gmail.com>
2023-06-01 12:16:54 +0530

    Adding Trie class in DS/Trie

--- Data Structures/Trie.cpp
+++ Data Structures/Trie.cpp
@@ -3,6 +3,13 @@ using namespace std;
 
 class TrieNode{
     public:
+    TrieNode *children[26];
+    bool isEndOfWord; 
+
+    TrieNode(){
+        isEndOfWord = false;
+        for(int i=0; i<26; i++) children[i] = NULL;
+    }
 };
 
 int main(){
