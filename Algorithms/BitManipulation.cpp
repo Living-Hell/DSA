9cfac0a8f6c970d19690d046a2ba7c339077a36d
Shushant Kumar <s.living.hell@gmail.com>
2023-06-14 21:50:10 +0530

    Adding isPowerofTwo in BitManipulation.cpp

--- Algorithms/BitManipulation.cpp
+++ Algorithms/BitManipulation.cpp
@@ -72,6 +72,11 @@ class BitManipulation{
         else return -1;
     }
 
+    // Function to check if given number n is a power of two.
+    bool isPowerofTwo(long long n){ 
+        return (n!=0) and (n&(n-1)) == 0;
+    }
+
 };
 
 int main(){
