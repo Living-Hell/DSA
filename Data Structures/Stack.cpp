65bd99ad6027d1d9426b14d17b4c5302092c3ca7
Shushant Kumar <s.living.hell@gmail.com>
2023-06-15 23:43:09 +0530

    Adding printList and reverseListItr in DS/Stack

--- Data Structures/Stack.cpp
+++ Data Structures/Stack.cpp
@@ -12,7 +12,39 @@ struct Node{
 
 class Stack{
     public:
-    
+    //Function to print the linked list
+    void printList(Node *node){
+        while(node){
+            cout<<node->data<<" ";
+            node = node->next;
+        }
+        cout<<endl;
+    }
+
+    //Function to reverse a given linked list of n nodes
+    //Iterative - Using stack
+    Node* reverseListItr(Node *node){
+        Node *rev; Node *temp;
+        rev = temp;
+        stack<int> st;
+        while(node){
+            st.push(node->data);
+            node = node->next;
+        }
+        while(!st.empty()){
+            int top = st.top();
+            st.pop();
+            temp->next = new Node(top);
+            temp = temp->next;
+        }
+        return rev->next;        
+    }
+
+    //Function to reverse a given linked list of n nodes
+    //Recursive solution
+    Node* reverseListRec(Node *node){
+        
+    }
 
 };
 
@@ -35,5 +67,8 @@ int main(){
         }
     }
 
+    st.printList(head);
+    Node *rev = st.reverseListItr(head);
+    st.printList(rev);
     return 0;
 }
