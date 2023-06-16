d31047fc72246c10396a542e475bf24186b77050
Shushant Kumar <s.living.hell@gmail.com>
2023-06-16 10:35:40 +0530

    Adding detectLoop in DS/Stack

--- Data Structures/Stack.cpp
+++ Data Structures/Stack.cpp
@@ -60,6 +60,18 @@ class Stack{
         return newHead;
     }
 
+    //Function to check if the linked list has a loop.
+    bool detectLoop(Node* head){
+        Node* slow = head;
+        Node* fast = head;
+        while(fast and fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast) return true;
+        }
+        return false;
+    }
+
 };
 
 int main(){
