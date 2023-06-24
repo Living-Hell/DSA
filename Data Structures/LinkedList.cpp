646f7fc7eddde45c202a42e87569a6e37abd8498
Shushant Kumar <s.living.hell@gmail.com>
2023-06-24 22:54:52 +0530

    Adding splitList in DS/LinkedList

--- Data Structures/LinkedList.cpp
+++ Data Structures/LinkedList.cpp
@@ -326,6 +326,22 @@ class LinkedList{
         return 1;
     }
 
+    //  function which splits the circular linked list.  head is pointer to head Node of given lined list.  
+    //head1_ref1 and *head_ref2 are pointers to head pointers of resultant two halves.
+    void splitList(Node *head, Node **head1_ref, Node **head2_ref){
+        Node* slow = head; Node* fast = head->next;
+        while(fast != head and fast->next != head){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        *head1_ref = head;
+        *head2_ref = slow->next;
+        slow->next = *head1_ref;
+        Node* temp = *head2_ref;
+        while(temp->next != head) temp = temp->next;
+        temp->next = *head2_ref;
+    }
+
 };
 
 int main(){
