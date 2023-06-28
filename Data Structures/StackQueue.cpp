1691059c82e25e80a7ded8856dbbbe4e581da42e
Shushant Kumar <s.living.hell@gmail.com>
2023-06-28 19:31:35 +0530

    Adding reverse in  DS/StackQueue.cpp

--- Data Structures/StackQueue.cpp
+++ Data Structures/StackQueue.cpp
@@ -217,7 +217,17 @@ class StackQueue{
         return st.empty();
     }
 
-    
+    //Function to reverse the string using stack.
+    char* reverse(char *S, int len){
+        stack<char> st;
+        for(int i=0; i<len; i++)
+            st.push(S[i]);
+        for(int i=0; i<len; i++){
+            S[i] = st.top();
+            st.pop();
+        }
+        return S;
+    }
 
 };
 
