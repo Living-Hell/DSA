ac7c2b0138e06f9a1c2aa805210ceada87c2f0c0
Shushant Kumar <s.living.hell@gmail.com>
2023-07-01 15:34:00 +0530

    Adding modifyQueue in DS/StackQueue.cpp

--- Data Structures/StackQueue.cpp
+++ Data Structures/StackQueue.cpp
@@ -626,6 +626,29 @@ public:
         }
         return ind == n;
     }
+
+    // Function to reverse first k elements of a queue.
+    queue<int> modifyQueue(queue<int> q, int k)
+    {
+        queue<int> ans;
+        stack<int> st;
+        while (k--)
+        {
+            st.push(q.front());
+            q.pop();
+        }
+        while (!st.empty())
+        {
+            ans.push(st.top());
+            st.pop();
+        }
+        while (!q.empty())
+        {
+            ans.push(q.front());
+            q.pop();
+        }
+        return ans;
+    }
 };
 
 int main()
