e3088fbe66b27ef9a2c8ee1fac4d15a880c32399
Shushant Kumar <s.living.hell@gmail.com>
2023-07-01 14:19:58 +0530

    Adding Implementation of stack using 2 queue in DS/StackQueue.cpp

--- Data Structures/StackQueue.cpp
+++ Data Structures/StackQueue.cpp
@@ -215,6 +215,41 @@ public:
     }
 };
 
+// Implementation of stack using 2 queue
+class QueueStack
+{
+private:
+    queue<int> q1;
+    queue<int> q2;
+
+public:
+    // Function to push an element into stack using two queues.
+    void push(int x)
+    {
+        q1.push(x);
+    }
+
+    // Function to pop an element from stack using two queues.
+    int pop()
+    {
+        if (q1.empty())
+            return -1;
+        while (q1.size() > 1)
+        {
+            q2.push(q1.front());
+            q1.pop();
+        }
+        int top = q1.front();
+        q1.pop();
+        while (!q2.empty())
+        {
+            q1.push(q2.front());
+            q2.pop();
+        }
+        return top;
+    }
+};
+
 class StackQueue
 {
 public:
