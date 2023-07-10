10152a2450df21981277535ba78bbdada4f4e546
Shushant Kumar <s.living.hell@gmail.com>
2023-07-10 23:20:35 +0530

    Adding maxStop in DS/Greedy

--- Data Structures/Greedy.cpp
+++ Data Structures/Greedy.cpp
@@ -241,6 +241,31 @@ public:
         }
         return ans;
     }
+
+    // Function to tell the maximum number of trains for which you can provide stoppage at your station
+    int maxStop(int n, int m, vector<vector<int>> &trains)
+    {
+        for (int i = 0; i < m; i++)
+            trains[i].push_back(i);
+        sort(trains.begin(), trains.end(),
+             [](vector<int> a, vector<int> b)
+             {
+                 if (a[1] == b[1])
+                     return a[3] < b[3];
+                 return a[1] < b[1];
+             });
+        vector<int> platform(n + 1, -1);
+        int ans = 0;
+        for (int i = 0; i < m; i++)
+        {
+            if (platform[trains[i][2]] <= trains[i][0])
+            {
+                ans++;
+                platform[trains[i][2]] = trains[i][1];
+            }
+        }
+        return ans;
+    }
 };
 
 int main()
