6590394e1273a3ed83c04586e11fba7eefd7c20f
Shushant Kumar <s.living.hell@gmail.com>
2023-07-19 16:27:45 +0530

    Adding removeInvalidParentheses in DS/Backtracking

--- Data Structures/Backtracking.cpp
+++ Data Structures/Backtracking.cpp
@@ -129,6 +129,52 @@ public:
         wordBreaker(n, s, st, "", ans);
         return ans;
     }
+
+    // Given a string s that contains parentheses and letters, remove the minimum number of invalid parentheses to make the input string valid.
+    // Return a list of unique strings that are valid with the minimum number of removals.
+    set<pair<int, string>> ipr;
+    void invalidParenthesesRemover(string s, unordered_set<string> &ans, int ind, int count)
+    {
+        pair<int, string> x = make_pair(ind, s);
+        if (ipr.find(x) != ipr.end())
+            return;
+        if (ind == s.size())
+        {
+            if (count == 0)
+                ans.insert(s);
+            return;
+        }
+        if (s[ind] == '(' or s[ind] == ')')
+        {
+            string temp = s;
+            temp.erase(ind, 1);
+            invalidParenthesesRemover(temp, ans, ind, count);
+        }
+        if (s[ind] == '(')
+            count++;
+        if (s[ind] == ')')
+            count--;
+        if (count < 0)
+            return;
+        ipr.insert(x);
+        invalidParenthesesRemover(s, ans, ind + 1, count);
+    }
+    vector<string> removeInvalidParentheses(string s)
+    {
+        unordered_set<string> ans;
+        invalidParenthesesRemover(s, ans, 0, 0);
+        int mx = 0;
+        for (string i : ans)
+        {
+            int sz = i.size();
+            mx = max(mx, sz);
+        }
+        vector<string> ret;
+        for (string i : ans)
+            if (i.size() == mx)
+                ret.push_back(i);
+        return ret;
+    }
 };
 
 int main()
