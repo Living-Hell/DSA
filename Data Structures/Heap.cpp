2e7b65d059bc1582ba988e104adcaad5cbe16ecb
Shushant Kumar <s.living.hell@gmail.com>
2023-07-24 16:45:44 +0530

    Adding mergeKArrays in DS/Heap

--- Data Structures/Heap.cpp
+++ Data Structures/Heap.cpp
@@ -4,17 +4,40 @@ using namespace std;
 class Heap
 {
 public:
-    //Function to find K largest elements from the array in decreasing order.
-	vector<int> kLargest(int arr[], int n, int k) {
-	    priority_queue<int> pq;
-	    for(int i=0; i<n; i++) pq.push(arr[i]);
-	    vector<int> ans;
-	    while(k--){
-	        ans.push_back(pq.top());
-	        pq.pop();
-	    }
-	    return ans;
-	}
+    // Function to find K largest elements from the array in decreasing order.
+    vector<int> kLargest(int arr[], int n, int k)
+    {
+        priority_queue<int> pq;
+        for (int i = 0; i < n; i++)
+            pq.push(arr[i]);
+        vector<int> ans;
+        while (k--)
+        {
+            ans.push_back(pq.top());
+            pq.pop();
+        }
+        return ans;
+    }
+
+    // Function to merge k sorted arrays.
+    vector<int> mergeKArrays(vector<vector<int>> arr, int K)
+    {
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for (auto i : arr)
+        {
+            for (int j : i)
+            {
+                pq.push(j);
+            }
+        }
+        vector<int> ans;
+        while (!pq.empty())
+        {
+            ans.push_back(pq.top());
+            pq.pop();
+        }
+        return ans;
+    }
 };
 
 int main()
