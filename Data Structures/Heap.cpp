1f5b0c98f72be02b20fd7c94e412d886f86815f8
Shushant Kumar <s.living.hell@gmail.com>
2023-07-24 15:54:24 +0530

    Adding kLargest in DS/Heap

--- Data Structures/Heap.cpp
+++ Data Structures/Heap.cpp
@@ -4,7 +4,17 @@ using namespace std;
 class Heap
 {
 public:
-    //
+    //Function to find K largest elements from the array in decreasing order.
+	vector<int> kLargest(int arr[], int n, int k) {
+	    priority_queue<int> pq;
+	    for(int i=0; i<n; i++) pq.push(arr[i]);
+	    vector<int> ans;
+	    while(k--){
+	        ans.push_back(pq.top());
+	        pq.pop();
+	    }
+	    return ans;
+	}
 };
 
 int main()
