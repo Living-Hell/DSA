ab7f9f6f55ccec472f5397612e61e2caceb41fb4
Shushant Kumar <s.living.hell@gmail.com>
2023-07-25 14:19:51 +0530

    Adding minCost in DS/Heap

--- Data Structures/Heap.cpp
+++ Data Structures/Heap.cpp
@@ -256,6 +256,26 @@ public:
         }
         return pq.top();
     }
+
+    // Function to return the minimum cost of connecting the ropes.
+    long long minCost(long long arr[], long long n)
+    {
+        priority_queue<long long, vector<long long>, greater<long long>> pq;
+        for (int i = 0; i < n; i++)
+            pq.push(arr[i]);
+        long long tcost = 0;
+        while (pq.size() > 1)
+        {
+            long long a = pq.top();
+            pq.pop();
+            long long b = pq.top();
+            pq.pop();
+            long long cost = a + b;
+            pq.push(cost);
+            tcost += cost;
+        }
+        return tcost;
+    }
 };
 
 int main()
