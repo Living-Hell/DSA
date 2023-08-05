8e894be766688dfdfd6aeee00fa62cb9f6aeb72b
Shushant Kumar <s.living.hell@gmail.com>
2023-08-05 16:48:46 +0530

    Adding leaders in DS/Array

--- Data Structures/Array.cpp
+++ Data Structures/Array.cpp
@@ -180,6 +180,23 @@ public:
         int sum = n * (n + 1) / 2;
         return sum - tsum;
     }
+
+    // Function to find the leaders in the array.
+    vector<int> leaders(int a[], int n)
+    {
+        int curr_max = INT_MIN;
+        vector<int> ans;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (a[i] >= curr_max)
+            {
+                ans.push_back(a[i]);
+                curr_max = a[i];
+            }
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
 };
 
 int main()
