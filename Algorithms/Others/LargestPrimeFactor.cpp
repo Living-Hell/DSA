b6e80f1694745d20d5d15f555e729d6321285d8c
Shushant Kumar <s.living.hell@gmail.com>
2023-08-09 21:13:18 +0530

    Adding LargestPrimeFactor in Algorithms

---
+++ Algorithms/Others/LargestPrimeFactor.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    // Function to find the largest prime factor of n using Algo Sieve of Eratosthenes.
+    long long int largestPrimeFactor(int n)
+    {
+        for (long long int i = 2; i * i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                while (n > i and n % i == 0)
+                    n /= i;
+            }
+        }
+        return n;
+    }
+};
