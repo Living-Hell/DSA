896f316e4e203933f1bbf5ce7d09a530c7fcb22f
Shushant Kumar <s.living.hell@gmail.com>
2023-08-10 20:35:12 +0530

    Adding RainWaterProblem in DS/Algo

--- Algorithms/Others/LargestPrimeFactor.cpp
+++ Algorithms/Others/LargestPrimeFactor.cpp
@@ -2,6 +2,7 @@ class Solution
 {
 public:
     // Function to find the largest prime factor of n using Algo Sieve of Eratosthenes.
+    //ToC = O(sqrt(n))
     long long int largestPrimeFactor(int n)
     {
         for (long long int i = 2; i * i <= n; i++)
